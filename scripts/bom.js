57c2f428ca4829eeb9c9ddbadb003b3388567c1b
abidwai <abidwai@cemtrexlabs.com>
2018-05-15 11:49:52 +0530

    add 'a' in console.log

--- scripts/bom.js
+++ scripts/bom.js
@@ -4,6 +4,6 @@
     var $safari_flag = $user_agent.toLowerCase().indexOf('safari');
     var $chrome_flag = $user_agent.toLowerCase().indexOf('chrome');
 
-    ($safari_flag !== -1 && $chrome_flag === -1) ? console.log("it\'s' safari only"): console.log("non safari");
+    ($safari_flag !== -1 && $chrome_flag === -1) ? console.log("it\'s' a safari only"): console.log("non safari");
 
 })();
