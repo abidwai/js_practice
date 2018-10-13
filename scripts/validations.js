3bd0c37e511ff08c6d2a04932357cbf5f49eca45
abidwai <abidwai@cemtrexlabs.com>
2018-10-13 18:02:19 +0530

    add validation js

---
+++ scripts/validations.js
@@ -0,0 +1,39 @@
+var contactUs = ((event) => {
+    var evt = EventUtil.getEvent(event);
+    var form = document.getElementById("form1");
+
+    var init = (evt) => {
+        console.log('contact us');
+        setEvent();
+    };
+
+    var validateFields = () => {
+        var status = checkEmpty();
+        if (status) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    var checkEmpty = () => {
+        return true;
+    };
+
+    var setEvent = (evt) => {
+        EventUtil.addHandler(form, "submit", (evt) => {
+            evt.preventDefault();
+            var status = validateFields();
+            if (status) {
+                form.submit();
+            }
+        });
+    };
+
+    return {
+        init: init
+    };
+
+})();
+
+EventUtil.addHandler(window, "load", contactUs.init);
