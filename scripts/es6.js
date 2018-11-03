40ad9d713475f475bfb7ea861caeb98032ca134c
abidwai <abidwai@cemtrexlabs.com>
2018-11-03 20:30:27 +0530

    es6

--- scripts/es6.js
+++ scripts/es6.js
@@ -1,6 +1,6 @@
-/**
- * map examples
- */
+////////////////////// 
+// map examples
+
 let numbers = [10, 20, 30, 40];
 
 let mapType1 = numbers.map(currentVal => 5 + currentVal);
@@ -11,9 +11,8 @@ let mapType2 = numbers.map((currentVal, index) => {
 });
 // console.log(mapType2);
 
-/**
- * arrays
- */
+////////////////////// 
+// array examples
 
 // ES6
 let digit = [10, 20, 30, 40];
@@ -21,9 +20,9 @@ let digit = [10, 20, 30, 40];
 //console.log(digit.find(currentVal => currentVal > 30));
 
 
-/**
- * spread operator examples
- */
+////////////////////// 
+// spread operator examples
+
 var addFoureAges = function(a, b, c, d) {
     return a + b + c + d;
 }
@@ -33,12 +32,16 @@ var sum = addFoureAges(10, 20, 30, 40);
 // ES5
 var ages = [10, 20, 30, 40];
 var sum5 = addFoureAges.apply(null, ages);
-console.log(sum5);
+// console.log(sum5);
 
 //ES6
 let sum6 = addFoureAges(...ages);
 // console.log(sum6);
 
-/**
- * 
- */
+////////////////////// 
+// rest operator examples
+
+let person = (limit, ...years) => {
+    years.forEach(ele => console.log(2018 - ele >= limit));
+}
+let anup = person(21, 1990, 2016);
