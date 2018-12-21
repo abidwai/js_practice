ab4bfa548518204a5e0846aa17338e65d74f5a8f
abidwai <abidwai@cemtrexlabs.com>
2018-12-21 16:28:40 +0530

    change variables name

--- scripts/bom.js
+++ scripts/bom.js
@@ -1,11 +1,11 @@
 (function() {
-	console.log("############# BOM #############");
+    console.log("############# BOM #############");
     var $user_agent = navigator.userAgent;
     console.log($user_agent);
 
-    var $safari_flag = $user_agent.toLowerCase().indexOf('safari');
-    var $chrome_flag = $user_agent.toLowerCase().indexOf('chrome');
+    var $safari = $user_agent.toLowerCase().indexOf('safari');
+    var $chrome = $user_agent.toLowerCase().indexOf('chrome');
 
-    ($safari_flag !== -1 && $chrome_flag === -1) ? console.log("it\'s' a safari only"): console.log("non safari");
+    ($safari !== -1 && $chrome === -1) ? console.log("it\'s' a safari only"): console.log("non safari");
 
 })();
