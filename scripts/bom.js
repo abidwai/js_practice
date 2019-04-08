e876e596034390a6df152e210acc5b1ba43f90d5
abidwai <abidwai@cemtrexlabs.com>
2019-04-08 15:09:15 +0530

    add comment

--- scripts/bom.js
+++ scripts/bom.js
@@ -5,7 +5,7 @@
 
     var $safari = $user_agent.toLowerCase().indexOf('safari');
     var $chrome = $user_agent.toLowerCase().indexOf('chrome');
-
+    // check safari client
     ($safari !== -1 && $chrome === -1) ? console.log("it\'s' a safari only"): console.log("non safari");
 
 })();
