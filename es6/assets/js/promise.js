c2ba8d5f7d060ba8d4f24e6e7b30e2120acb1634
Anup Bidwai <bidwaiav@gmail.com>
2020-04-18 17:59:02 +0530

    add promise js

---
+++ es6/assets/js/promise.js
@@ -0,0 +1,46 @@
+// Morning promise
+let morning = function () {
+    return new Promise(function (resolve, reject) {
+        if (false) {
+            resolve(`morning resolve,`);
+        } else {
+            reject(`morning reject,`);
+        }
+    });
+}
+
+// afternoon promise
+let afternoon = function (message) {
+    return new Promise(function (resolve, reject) {
+        if (true) {
+            resolve(`${message} afternoon resolve,`);
+        } else {
+            reject(`${message} afternoon reject,`);
+        }
+    });
+}
+
+// night promise
+let night = function (message) {
+    return new Promise(function (resolve, reject) {
+        resolve(`${message} night resolve,`);
+    });
+}
+
+
+
+morning().then(function (result) {
+    // resolve area
+    return afternoon(result);
+}, function (result) {
+    // reject area
+    return afternoon(result);
+}).then(function (result) {
+    // resolve area
+    return night(result);
+}, function (result) {
+    // reject area
+    return night(result);
+}).then(function (result) {
+    console.log(`${result} finished`);
+});
